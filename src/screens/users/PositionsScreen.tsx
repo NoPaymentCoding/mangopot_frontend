6d9069bfb41d94046079fb73d3c5c67750c1f77c
YujeongLEE <dldbwjd24@ewhain.net>
2023-01-25 23:30:18 +0900

    feat : position 선택한 포지션 리스트업

--- src/screens/users/PositionsScreen.tsx
+++ src/screens/users/PositionsScreen.tsx
@@ -1,16 +1,6 @@
 //react libraries import
 import React, {useEffect, useState} from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  SafeAreaView,
-  ScrollView,
-  FlatList,
-  Image,
-  StyleSheet,
-  Pressable,
-} from 'react-native';
+import {FlatList, Image, Pressable, SafeAreaView, StyleSheet, Text, TouchableOpacity, View,} from 'react-native';
 
 //design 관련
 import styles from './style';
@@ -21,96 +11,110 @@ import positions from '../../types/users/SignUpTypes';
 
 //stack 관련 import
 import {StackScreenProps} from '@react-navigation/stack';
-import interests from '../../types/users/SignUpTypes';
 import userService from '../../services/userService';
 
-export type PositionsScreenProps = StackScreenProps<
-  UserStackParamList,
-  'Positions'
->;
+export type PositionsScreenProps = StackScreenProps<UserStackParamList,
+    'Positions'>;
 
-const Item = ({name}: string) => {
-  const [isSelected, setIsSelected] = useState<boolean>(false);
-  return (
-    <Pressable
-      style={customStyles.selectBoxArea}
-      onPress={() => {
-        setIsSelected(!isSelected);
-      }}>
-      <View style={{flex: 8}}>
-        <Text style={customStyles.selectTxt}>{name}</Text>
-      </View>
-      {isSelected ? (
-        <View style={{flex: 1, justifyContent: 'center'}}>
-          <Image source={require('../../assets/images/check.png')} />
-        </View>
-      ) : (
-        <View />
-      )}
-    </Pressable>
-  );
+type Props = {
+    name: string;
+    id: number;
+    appendPositionsList: (x: number) => void;
+}
+
+
+const Item = ({name, id, appendPositionsList}: Props) => {
+    const [isSelected, setIsSelected] = useState<boolean>(false);
+    return (
+        <Pressable
+            style={customStyles.selectBoxArea}
+            onPress={() => {
+                setIsSelected(!isSelected);
+                appendPositionsList(id.posId);
+            }}>
+            <View style={{flex: 8}}>
+                <Text style={customStyles.selectTxt}>{name}</Text>
+            </View>
+            {isSelected ? (
+                <View style={{flex: 1, justifyContent: 'center'}}>
+                    <Image source={require('../../assets/images/check.png')}/>
+                </View>
+            ) : (
+                <View/>
+            )}
+        </Pressable>
+    );
 };
 
 export default function PositionsScreen({navigation}: PositionsScreenProps) {
-  const [positionsInfo, setPositionsInfo] = useState<positions[]>([]);
+    const [positionsInfo, setPositionsInfo] = useState<positions[]>([]);
 
-  useEffect(() => {
-    userService
-      .getPositionList()
-      .then(res => {
-        setPositionsInfo(res.data);
-      })
-      .catch(reason => {
-        console.log(reason);
-      });
-  }, []);
+    let selectedPositions: number[] = [];
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.topArea}>
-        <View style={styles.titleTxtArea}>
-          <Text style={styles.titleTxt}>
-            {'관심있는 포지션을\n선택해주세요'}
-          </Text>
+    const appendPositionsList = (x: number) => {
+        selectedPositions.push(x);
+        // selectedPositions.forEach(function (number) {
+        //     console.log(number);
+        // });
+    }
+
+
+    useEffect(() => {
+        userService
+            .getPositionList()
+            .then(res => {
+                setPositionsInfo(res.data);
+            })
+            .catch(reason => {
+                console.log(reason);
+            });
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.topArea}>
+                <View style={styles.titleTxtArea}>
+                    <Text style={styles.titleTxt}>
+                        {'관심있는 포지션을\n선택해주세요'}
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.midArea}>
+                <SafeAreaView style={{flex: 2}}>
+                    <FlatList
+                        data={positionsInfo}
+                        renderItem={({item}) => <Item name={item.name} id={item}
+                                                      appendPositionsList={appendPositionsList}/>}
+                    />
+                </SafeAreaView>
+            </View>
+            <View style={styles.btmArea}>
+                <View style={styles.btnArea}>
+                    <TouchableOpacity
+                        style={styles.btn}
+                        onPress={() => navigation.navigate('Profile')}>
+                        <Text style={styles.btnTxt}>{'다음으로'}</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.btnArea}/>
+            </View>
         </View>
-      </View>
-      <View style={styles.midArea}>
-        <SafeAreaView style={{flex: 2}}>
-          <ScrollView>
-            <FlatList
-              data={positionsInfo}
-              renderItem={({item}) => <Item name={item.name} />}
-            />
-          </ScrollView>
-        </SafeAreaView>
-      </View>
-      <View style={styles.btmArea}>
-        <View style={styles.btnArea}>
-          <TouchableOpacity
-            style={styles.btn}
-            onPress={() => navigation.navigate('Profile')}>
-            <Text style={styles.btnTxt}>{'다음으로'}</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.btnArea} />
-      </View>
-    </View>
-  );
+    );
 }
 
 const customStyles = StyleSheet.create({
-  selectBoxArea: {
-    flex: 1,
-    flexDirection: 'row',
-    borderBottomWidth: 2,
-    borderColor: '#F0F0F0',
-    margin: 10,
-  },
-  selectTxt: {
-    padding: 10,
-    fontSize: 20,
-    paddingBottom: 15,
-  },
+    selectBoxArea: {
+        flex: 1,
+        flexDirection: 'row',
+        borderBottomWidth: 2,
+        borderColor: '#F0F0F0',
+        margin: 10,
+    },
+    selectTxt: {
+        padding: 10,
+        fontSize: 20,
+        paddingBottom: 15,
+    },
 });
 
 // const positionsInfo: positions[] = [
