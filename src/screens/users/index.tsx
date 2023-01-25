02b1146b47ee6cce8ef445e1dad3562657a24235
YujeongLEE <dldbwjd24@ewhain.net>
2023-01-25 23:31:16 +0900

    design : splash화면 구조변경

--- src/screens/users/index.tsx
+++ src/screens/users/index.tsx
@@ -1,6 +1,6 @@
 //react libraries import
 import React from 'react';
-import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {Image, StyleSheet, TouchableOpacity, View} from 'react-native';
 
 //design 관련
 import styles from './style';
@@ -14,49 +14,49 @@ import {StackScreenProps} from '@react-navigation/stack';
 export type SplashScreenProps = StackScreenProps<UserStackParamList, 'Splash'>;
 
 export default function Users({navigation}: SplashScreenProps) {
-  return (
-    <View style={styles.container}>
-      <View style={styles.topArea} />
-      <View style={customStyles.midArea}>
-        <View style={customStyles.logoArea}>
-          <Image
-            source={require('../../assets/images/mangopot_splash_orange.png')}
-            style={{flex: 0.6, resizeMode: 'contain'}}
-          />
+    return (
+        <View style={styles.container}>
+            <View style={styles.topArea}/>
+            <View style={customStyles.midArea}>
+                <View style={customStyles.logoArea}>
+                    <Image
+                        source={require('../../assets/images/mangopot_splash_orange.png')}
+                        style={{flex: 0.6, resizeMode: 'contain'}}
+                    />
+                </View>
+            </View>
+            <View style={styles.btmArea}>
+                {/*<View style={styles.btnArea} />*/}
+                <View style={styles.btnArea}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Nickname')}>
+                        <Image
+                            source={require('../../assets/images/kakao_login_large_wide.png')}
+                            style={{
+                                resizeMode: 'contain',
+                                flex: 1,
+                                maxWidth: '100%',
+                                //  가로폭 맞추기 maxWidth : 100% / 세로폭 맞추기 : resizeMode : 'contain'
+                            }}
+                        />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.btnArea}/>
+            </View>
         </View>
-      </View>
-      <View style={styles.btmArea}>
-        {/*<View style={styles.btnArea} />*/}
-        <View style={styles.btnArea}>
-          <TouchableOpacity onPress={() => navigation.navigate('Nickname')}>
-            <Image
-              source={require('../../assets/images/kakao_login_large_wide.png')}
-              style={{
-                resizeMode: 'contain',
-                flex: 1,
-                maxWidth: '100%',
-                //  가로폭 맞추기 maxWidth : 100% / 세로폭 맞추기 : resizeMode : 'contain'
-              }}
-            />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.btnArea} />
-      </View>
-    </View>
-  );
+    );
 }
 
 const customStyles = StyleSheet.create({
-  logoArea: {
-    flex: 0.7,
-    justifyContent: 'center',
-    alignItems: 'center',
-    // backgroundColor: 'pink',
-  },
-  midArea: {
-    flex: 3,
-    justifyContent: 'center',
-    // backgroundColor: 'green',
-    padding: 20,
-  },
+    logoArea: {
+        flex: 0.7,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: 'pink',
+    },
+    midArea: {
+        flex: 3,
+        justifyContent: 'center',
+        // backgroundColor: 'green',
+        padding: 20,
+    },
 });
